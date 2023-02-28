9d829264a5589107d9b4195ecb2cc35543051e3a
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2023-02-28 12:58:17 +0530

    product added

--- models/Ad.model.js
+++ models/Ad.model.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 
-const adSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  image: { type: String, required: true },
-  location: { type: String, required: true },
-  postedAt: { type: String, required: true },
-  price: { type: String, required: true },
-});
+const adSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    image: { type: String, required: true },
+    location: { type: String, required: true },
+    postedAt: { type: String, required: true },
+    price: { type: String, required: true },
+  },
+  {
+    virsionKey: false,
+  }
+);
 
 const AdModel = mongoose.model("ad", adSchema);
 
--- routes/ad.route.js
+++ routes/ad.route.js
@@ -13,4 +13,17 @@ ad.get("/", async (req, res) => {
     res.send("somthing went wrong");
   }
 });
+ad.post("/", async (req, res) => {
+  let payload = req.body;
+  try {
+    console.log(payload);
+    await new AdModel(payload).save();
+    res.status(200).send("post data");
+  } catch (err) {
+    res.status(400).send("something went wrong");
+    console.log(err);
+  }
+});
 module.exports = { ad };
+//status(200).
+//status(400).
