1dc158eec2e4b8006b296d8e91b7986730537ae7
Praful-87 <103850217+Praful-87@users.noreply.github.com>
2023-02-28 15:36:42 +0530

    all done

--- routes/ad.route.js
+++ routes/ad.route.js
@@ -11,7 +11,6 @@ ad.get("/", async (req, res) => {
   // console.log(page);
   if (!page) page = 0;
   if (!limit) limit = 4;
-  if (!filter) limit = 0;
 
   // console.log(page, limit,page*limit);
   // res.send("ok");
